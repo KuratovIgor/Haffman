4f9cbe00b20b2d3b2dcbc523ac678bcf28c5725f
KuratovIgor <kuratovi@bk.ru>
2021-04-27 18:03:55 +0700

    Добавьте файлы проекта.

---
+++ HaffmanLibrary/List.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaffmanLibrary
+{
+    class List : IComparable<List>
+    {
+        private char _value = '\0';
+        private int _weight = 0;
+        private List left = null;
+        private List right = null;
+        private string _code = null;
+
+        public List Left
+        {
+            get => left;
+            set => left = value;
+        }
+
+        public List Right
+        {
+            get => right;
+            set => right = value;
+        }
+
+        public string Code
+        {
+            get => _code;
+            set => _code = value;
+        }
+
+        public int Weight { get => _weight; }
+        public char Value { get => _value; }
+
+        public List(in char value, in int weight)
+        {
+            this._value = value;
+            this._weight = weight;
+        }
+
+        public List(in int weight)
+        {
+            this._weight = weight;
+        }
+
+        public int CompareTo(List obj)
+        {
+            return this._weight.CompareTo(obj._weight);
+        }
+    }
+}
